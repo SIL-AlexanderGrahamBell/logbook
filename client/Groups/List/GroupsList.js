c26130ce2553ee5ee52596694288713f12c4a90f
Christophe Anthon <work@anthon.lu>
2015-08-22 23:09:09 +0200

    adding new design

--- client/Groups/List/GroupsList.js
+++ client/Groups/List/GroupsList.js
@@ -2,12 +2,6 @@ Meteor.subscribe("Groups");
 
 Template.GroupsListMember.helpers({
     groups: function() {
-
         return App.Collections.Groups.find({owner: Meteor.userId()}).fetch();
-        Meteor.call("Groups:own", function(error, result){
-            if(result){
-                return result;
-            }
-        });
     }
 });
