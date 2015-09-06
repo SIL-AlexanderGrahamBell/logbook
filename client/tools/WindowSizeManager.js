8bb49d288cea1bb74322800fac295ba0dabcf9b9
Christophe Anthon <work@anthon.lu>
2015-09-06 12:10:23 +0200

    user wants to be able to edit group

--- client/tools/WindowSizeManager.js
+++ client/tools/WindowSizeManager.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
     App.Actions.resize();
-    console.log('startup');
+
     window.addEventListener('resize', function(event){
         App.Actions.resize();
     });
@@ -12,7 +12,7 @@ App.Actions.resize = function() {
     var publicHeight = $('.public').height();
     var contentHeight = $('.content').height();
     var menuHeight = $('.main-sidebar').height();
-    console.log(viewportHeight + " > "+ Math.max(contentHeight));
+
     if (viewportHeight > Math.max(contentHeight) + headerHeight) {
         $('.public, .content-wrapper, .main-sidebar').css('min-height', viewportHeight - headerHeight);
     } else {
